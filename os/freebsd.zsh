ed3f92c64133d644951242044473c3266bc504f3
Yanhui Shen <shen.elf@gmail.com>
2018-05-08 12:11:13 +0800

    Add pkg and port related command

--- os/freebsd.zsh
+++ os/freebsd.zsh
@@ -21,3 +21,19 @@ function svn-co-port() {
 }
 
 alias svn-diff-port="svn diff > ../`make -VPKGNAME`.diff"
+
+alias pkg-msg="pkg info -D"
+alias pkg-fresh='pkg version -vl "<"'
+
+alias port-init='portsnap fetch extract'
+alias port-sync='portsnap fetch update'
+alias port-rebuild='portmaster -d'
+alias port-update='portmaster -w -D -a'
+alias port-clean='portmaster --clean-distfiles -y'
+alias port-notice='$PAGER /usr/ports/UPDATING'
+alias port-dep="\
+echo '$fg_bold[white]build-depends-list:$reset_color' && make build-depends-list | sort && \
+echo '$fg_bold[white]run-depends-list:$reset_color' && make run-depends-list | sort && \
+echo '$fg_bold[red]missing:$reset_color' && make missing | sort"
+
+alias minstall="make install clean"

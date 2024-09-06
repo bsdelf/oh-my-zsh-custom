28a346e62211f4341b84f172eaef83f01a30e9b5
Yanhui Shen <shen.elf@gmail.com>
2024-09-06 18:39:42 +0800

    Cleanup path

--- index.zsh
+++ index.zsh
@@ -14,3 +14,5 @@ try_source "$ZSH_CUSTOM/os/$(uname -s | tr '[:upper:]' '[:lower:]').zsh"
 try_source "$ZSH_CUSTOM/os/generic.after.zsh"
 try_source "$HOME/.zshrc.local"
 try_source "$HOME/.zshrc.private"
+
+typeset -U PATH

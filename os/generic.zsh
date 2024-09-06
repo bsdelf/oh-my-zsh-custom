f7123c44d3e48e0da4b2634fa115b9315aae9651
Yanhui Shen <shen.elf@gmail.com>
2024-09-06 18:27:07 +0800

    Add docker-swagger command

--- os/generic.zsh
+++ os/generic.zsh
@@ -256,3 +256,58 @@ function docker-volume-purge() {
         docker volume rm $(printf "$ids")
     fi
 }
+
+function docker-swagger() {
+    if [ ! -f "$1" ]; then
+        echo "File does not exist!"
+        return 1
+    fi
+    file=$(realpath "$1")
+
+    port=""
+    for i in $(seq 1 3); do
+        port=$(shuf -i 2000-65000 -n 1)
+        nc -w 2 -vz localhost $port 2>&1 | grep -q refused
+        if [ $? -eq 0 ]; then
+            break
+        else
+            port=""
+        fi
+    done
+    if [ -z $port ]; then
+        echo "No port available!"
+        return 1
+    fi
+    url="http://localhost:$port"
+
+
+    name=$(printf "swagger-%s" $(uuidgen))
+    function cleanup {
+        trap - SIGHUP SIGINT SIGTERM
+        echo "Stopping swagger..."
+        docker kill $name
+    }
+    trap cleanup SIGHUP SIGINT SIGTERM
+    echo "Launching $name with $1"
+    docker run --name $name -p $port:8080 -e SWAGGER_JSON=/api.yaml -v "$file":/api.yaml --rm swaggerapi/swagger-ui &
+    pid=$!
+
+    for i in $(seq 1 10); do
+        if ps -p $pid > /dev/null; then
+            nc -w 2 -vz localhost $port 2>&1 | grep -q open
+            if [ $? -eq 0 ]; then
+                break
+            else
+                echo "Waiting swagger to be ready..."
+                sleep 1
+            fi
+        else
+            echo "Failed to launch swagger!"
+            return 1
+        fi
+    done
+
+    echo "Please visit: $url"
+    xdg-open "$url"
+    wait $pid
+}
